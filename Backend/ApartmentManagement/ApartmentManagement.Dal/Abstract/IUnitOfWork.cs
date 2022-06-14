9f01419c8e68dbd66f7640ccd3d3d68f419528e9
Ümmühan Keleş <ummuhankeles4@gmail.com>
2022-06-14 18:23:03 +0300

    Add the unit of work pattern

---
+++ Backend/ApartmentManagement/ApartmentManagement.Dal/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ApartmentManagement.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmentManagement.Dal.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> GetRepository<T>() where T : EntityBase;
+
+        bool BeginTransection(); // transection başlatma
+        bool RollBackTransection(); // hata durumunda sürecin geri alınmasını sağlayan işlem
+        int SaveChanges(); // transection onaylama
+    }
+}
